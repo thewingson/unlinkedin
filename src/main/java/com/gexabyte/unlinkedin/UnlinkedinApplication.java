c10c0df236e52970c142473009b340bd4e47af2b
Almat <almatrakh97@gmail.com>
2020-06-02 12:49:13 +0600

    Initial commit

---
+++ src/main/java/com/gexabyte/unlinkedin/UnlinkedinApplication.java
@@ -0,0 +1,13 @@
+package com.gexabyte.unlinkedin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnlinkedinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnlinkedinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gexabyte/unlinkedin/UnlinkedinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gexabyte.unlinkedin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnlinkedinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
